7460381919d40639110d79a53104e56199e67583
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-02-09 15:11:14 +0100

    Creating an exception for images not found

---
+++ projet3/src/main/java/com/openclassrooms/projet3/exception/ImageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.projet3.exception;
+
+public class ImageNotFoundException extends Exception{
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
