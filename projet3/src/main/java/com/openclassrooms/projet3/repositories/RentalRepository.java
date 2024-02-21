69976c9c0720863d0682aeecfe8f1c393efc85b2
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-02-21 21:46:43 +0100

    Add @Repository

--- projet3/src/main/java/com/openclassrooms/projet3/repositories/RentalRepository.java
+++ projet3/src/main/java/com/openclassrooms/projet3/repositories/RentalRepository.java
@@ -2,6 +2,8 @@ package com.openclassrooms.projet3.repositories;
 
 import com.openclassrooms.projet3.entites.RentalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RentalRepository extends JpaRepository<RentalEntity, Integer> {
 }
--- projet3/src/main/java/com/openclassrooms/projet3/repositories/UserRepository.java
+++ projet3/src/main/java/com/openclassrooms/projet3/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.openclassrooms.projet3.repositories;
 
 import com.openclassrooms.projet3.entites.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     // Les méthodes findAll et findById sont générées automatiquement par Spring Data JPA
 
